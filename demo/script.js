695582ba6cba25a51c79f43a03674f468e22d333
jt3k <iyntx@ya.ru>
2016-03-11 02:52:19 +0300

    fix implementation
    and update demo

--- demo/script.js
+++ demo/script.js
@@ -1,4 +1,4 @@
-(function (root, factory) {
+(function(root, factory) {
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
     define([], factory);
@@ -11,82 +11,65 @@
     // Browser globals (root is window)
     root.fuse = factory();
   }
-}(this, function () {
-
-  function fuse(one, two, p) {
+}(this, function() {
+  /**
+   * @param  {String}  one           first color
+   * @param  {String}  two           second hex color value
+   * @param  {Number}  percentage    float value of percentage
+   * @return  {String}               hex color corresponds to the percentage
+   *                                     ratio between the two colors
+   */
+  function fuse(one, two, percentage) {
     /**
-     * Return  percentage between first and second arguments
-     * @param  {String}  o  first hex value
-     * @param  {String}  t  second hex-value
+     * Return delta percentage between first and second hex
+     * @param  {String}  o  first hex value [00-ff]
+     * @param  {String}  t  second hex-value [00-ff]
      * @param  {Number}  p  percentage value. Float between 0 and 1
-     * @return  {String}  hex value percentage between first and second values
+     * @return {String}     hex value percentage between first and second values
+     *                          if one = 100, two = 200, p = .5 
+     *                          then fuse(100,200,.5); //=> 150
      */
     function fus(o, t, p) {
       if (o === t) {
-        //значения равны, нечего вычислят
+        // values are equal. there is nothing to calculate
         return o;
       }
-
+      // convert from hex
       o = parseInt(o, 16);
       t = parseInt(t, 16);
 
-      var min, max, res;
-      if (o < t) {
-        max = t;
-        min = o;
-      } else {
-        var invert = true;
-        max = o;
-        min = t;
-      }
+      // percentage of delta
+      var res = ((t - o) * p) + o;
 
-
-      var res = ((max - min) * p) + min;
-
-      if (invert) {
-        console.log(res);
-        res = max - res;
-      }
+      // remove float point
       res = Math.round(res);
+      // convert to hex
       res = res.toString(16);
-      return ('0' + res).slice(-2);
+      // add a leaad zero
+      res = ('0' + res).slice(-2);
+
+      return res;
     }
 
     function stringToColorArray(color) {
-      return [color.slice(0, 2), color.slice(2, 4), color.slice(4, 6)];
+      // split string in two chars chunks 
+      return color.match(/.{2}/g);
     }
 
+    // remove hash symbol
     one = one.replace('#', '');
     two = two.replace('#', '');
 
+    // convert to array
     one = stringToColorArray(one);
     two = stringToColorArray(two);
 
-    var three = '';
-    one.forEach(function(el, index) {
-      three += fus(one[index], two[index], p);
-    });
-
-    return three;
+    // concat calculated values of each rgb component
+    return one.reduce(function(memo, el, index) {
+      memo += fus(one[index], two[index], percentage);
+      return memo;
+    }, '');
   }
 
   return fuse;
 }));
-
-//////////////////////////////////////////
-/// INIT
-var one = document.querySelector('.one');
-var two = document.querySelector('.two');
-var pzt = document.querySelector('.percentage');
-var bod = document.body;
-
-function go() {
-  'use strict';
-  var color = `#${fuse(one.value, two.value, pzt.value)}`;
-  console.log(color);
-  bod.style.background = color;
-}
-
-pzt.oninput = one.oninput = two.oninput = go;
-
-go();
